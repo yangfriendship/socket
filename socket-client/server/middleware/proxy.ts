b32fa008940994701903e110485e9514eda187f8
Jongmin Yoon <jongmin4943@gmail.com>
2023-08-21 22:18:16 +0900

    fix(client) : proxyRequest origin 설정 추가

--- socket-client/server/middleware/proxy.ts
+++ socket-client/server/middleware/proxy.ts
@@ -1,5 +1,3 @@
-import { RequestHeaders } from "h3";
-
 const { apiServerUrl, apiEndpoint } = useRuntimeConfig().public;
 export default defineEventHandler(event => {
 	const targetUrl = event.node.req.url;
@@ -7,11 +5,11 @@ export default defineEventHandler(event => {
 
 	if (targetUrl.startsWith(apiEndpoint)) {
 		const target = new URL(targetUrl, apiServerUrl as string);
-		const headers: RequestHeaders = {
-			host: target.host,
-		};
 		return proxyRequest(event, target.toString(), {
-			headers,
+			headers: {
+				host: target.host,
+				origin: target.origin,
+			},
 		});
 	}
 });
