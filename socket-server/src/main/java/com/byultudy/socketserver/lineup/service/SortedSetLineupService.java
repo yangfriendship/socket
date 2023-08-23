cbf9136a7dfec047dff54d358cd32cefcc970cb1
yangfriendship <yangfriendship@naver.com>
2023-08-24 01:13:46 +0900

    refactor: RedisScript 반환 타입 변경 및 코드정리
    - String -> Long 타입 변경
    - 불필요한 메서드 삭제

--- socket-server/src/main/java/com/byultudy/socketserver/lineup/service/SortedSetLineupService.java
+++ socket-server/src/main/java/com/byultudy/socketserver/lineup/service/SortedSetLineupService.java
@@ -5,7 +5,6 @@ import com.byultudy.socketserver.lineup.dto.LineupResponse;
 import com.byultudy.socketserver.lineup.exception.LineupFinishException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.RedisScript;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -15,6 +14,17 @@ public class SortedSetLineupService implements LineupService {
     public static final String ZSET_KEY_PREFIX = "line:zset:";
 
     public static final String STOCK_KEY_PREFIX = "line:stock:";
+
+    private static final RedisScript<Long> IF_HAS_STOCK_INCR = RedisScript.of(
+            """
+                    local currentStock = redis.call('GET',KEYS[1]);
+                    local result = 0;
+                    if tonumber(currentStock) > 0 then
+                        result = redis.call('decr', KEYS[1]);
+                    end;
+                    return result;
+                    """, Long.class
+    );
 
     public SortedSetLineupService(final RedisTemplate<String, String> redisTemplate) {
         this.redisTemplate = redisTemplate;
@@ -34,31 +44,9 @@ public class SortedSetLineupService implements LineupService {
     }
 
     private boolean isFinish(final LineupRequest request) {
-        final RedisScript<String> script = RedisScript.of(
-                """
-                        local currentStock = redis.call('GET',KEYS[1]);
-                        local result = 0;
-                        if tonumber(currentStock) > 0 then
-                            result = redis.call('decr', KEYS[1]);
-                        end;
-                        return tostring(result);
-                        """, String.class
-        );
-        final String result = this.redisTemplate.execute(script
-                , List.of(this.genKey(STOCK_KEY_PREFIX, request)));
-        if (!StringUtils.hasLength(result) || !isNumeric(result)) {
-            return true;
-        }
-        return Long.parseLong(result) < 1;
-    }
-
-    private boolean isNumeric(final String stock) {
-        try {
-            Long.valueOf(stock);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        final Long result = this.redisTemplate.execute(
+                IF_HAS_STOCK_INCR, List.of(this.genKey(STOCK_KEY_PREFIX, request)));
+        return result == null || result < 1;
     }
 
     protected String genZSetKey(final LineupRequest request) {
@@ -67,10 +55,6 @@ public class SortedSetLineupService implements LineupService {
 
     protected String genKey(final String keyPrefix, final LineupRequest request) {
         return keyPrefix + request.getLineId();
-    }
-
-    private double getCurrentTime() {
-        return 0d;
     }
 
 }
